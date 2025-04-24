e1db2b2086ac49f4e1e07d6ec75064adca34e106
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-04-24 11:22:49 +0530

    Update appwrite.ts

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -4,7 +4,7 @@ const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
-  .setEndpoint(`https://cloud.appwrite.io/v1`)
+  .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const database = new Databases(client);
@@ -17,7 +17,6 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
 
     if (result.documents.length > 0) {
       const existingMovie = result.documents[0];
-
       await database.updateDocument(
         DATABASE_ID,
         COLLECTION_ID,
@@ -38,5 +37,21 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
   } catch (error) {
     console.error(error);
     throw error;
+  }
+};
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+  } catch (error) {
+    console.error(error);
+    return undefined;
   }
 };
